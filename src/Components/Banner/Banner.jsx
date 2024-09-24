f85f189980c28558e781b5fb106894120aae2d11
Sofi <sofiiay15@mail.ru>
2024-09-25 01:20:00 +0400

    Changed Welcome part and started Nav

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,6 +3,7 @@ import {AiFillGithub} from 'react-icons/ai';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import styles from '../Banner/Banner.module.css'
 
 const Banner = () => {
     useEffect(() => {
@@ -13,22 +14,22 @@ const Banner = () => {
             })
     },[])
     return (
-        <div data-aos="fade-down" className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg-text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center">
-            <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text">
-                <h1 data-aos="fade-down" className="text-[52px] font-semibold mb-8 leading-normal"><span className="text-gray-900 uppercase">Welcome to my Personal Website</span></h1>
-                <p className="font-serif text-2xl">Hi! Here, you’ll not only explore my programming journey, skills, and projects but also discover some of my favorite things that inspire me every day. From coding challenges to my favorite books, music, and quotes, this portfolio is a reflection of both my technical growth and personal interests.</p>
-                <div className="flex items-center justify-center">
-                    <div className="flex mt-8 gap-2">
-                        <a href="https://github.com/Ssofi1a" className="bg-red-100 hover:bg-red-300 rounded-full glow p-2 cursor-pointer" target="_blank" rel="noopener noreferrer">
+        <div data-aos="fade-down" className={styles.home_section}>
+            <div className={styles.text_section}>
+                <h1 data-aos="fade-down" className={styles.greeting}>Welcome to my Personal Website</h1>
+                <p data-aos="fade-down" className={styles.text}>Hi! I'm <span className={styles.name}>Sofi Abovyan</span>. Here, you’ll not only explore my programming journey, skills, and projects but also discover some of my favorite things that inspire me every day. From coding challenges to my favorite books, music, and quotes, this portfolio is a reflection of both my technical growth and personal interests.</p>
+                <div className={styles.home_content}>
+                    <div className={styles.profiles}>
+                        <a href="https://github.com/Ssofi1a" className={styles.links} target="_blank" rel="noopener noreferrer">
                             <AiFillGithub className="text-[38px]"/>
                         </a>
-                        <a href="https://www.linkedin.com/in/sofi-abovyan-a9557025a/" className="bg-red-100 hover:bg-red-300 rounded-full glow p-2 cursor-pointer" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.linkedin.com/in/sofi-abovyan-a9557025a/" className={styles.links} target="_blank" rel="noopener noreferrer">
                             <FaLinkedin className="text-[38px]"/>
                         </a>
-                        <a href="https://www.instagram.com/__sofi1a__/" className="bg-red-100 hover:bg-red-300 rounded-full glow p-2 cursor-pointer" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.instagram.com/__sofi1a__/" className={styles.links} target="_blank" rel="noopener noreferrer">
                             <FaInstagram className="text-[38px]"/>
                         </a>
-                        <a href="https://www.facebook.com/sofulikabovyan2003" className="bg-red-100 hover:bg-red-300 rounded-full glow p-2 cursor-pointer" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.facebook.com/sofulikabovyan2003" className={styles.links} target="_blank" rel="noopener noreferrer">
                             <FaFacebook className="text-[38px]"/>
                         </a>
                     </div>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-scroll";
 import { FaTimes } from "react-icons/fa";
 import { CiMenuFries } from "react-icons/ci";
+import styles from '../Nav/Nav.module.css'
 
 const Nav = () => {
     const [click, setClick] = useState(false);
@@ -9,35 +10,35 @@ const Nav = () => {
         setClick(!click);
     }
     const content = <> 
-        <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-red-100 transition ">
-            <ul className="text-center text-xl p-20">
+        <div className={styles.navbar}>
+            <div className={styles.menu}>
+                <ul>
                 <Link spy={true} smooth={true} to="Home">
-                <li className="my-4 py-4 border-b bg-red-100 hover:bg-red-300 hover:border">Home</li>
-                </Link>
-                <Link spy={true} smooth={true} to="About">
-                <li className="my-4 py-4 border-b bg-red-100 hover:bg-red-300 hover:border">About</li>
-                </Link>
-                <Link spy={true} smooth={true} to="Skills">
-                <li className="my-4 py-4 border-b bg-red-100 hover:bg-red-300 hover:border">Skills</li>
-                </Link>
-                <Link spy={true} smooth={true} to="Projects">
-                <li className="my-4 py-4 border-b bg-red-100 hover:bg-red-300 hover:border">Projects</li>
-                </Link>
-                <Link spy={true} smooth={true} to="Favourite">
-                <li className="my-4 py-4 border-b bg-red-100 hover:bg-red-300 hover:border">Favourite</li>
-                </Link>
-                <Link spy={true} smooth={true} to="Contact">
-                <li className="my-4 py-4 border-b bg-red-100 hover:bg-red-300 hover:border">Contact</li>
-                </Link>
-            </ul>
+                            <li className="menu-item">Home</li>
+                        </Link>
+                        <Link spy={true} smooth={true} to="About">
+                            <li className="menu-item">About</li>
+                        </Link>
+                        <Link spy={true} smooth={true} to="Skills">
+                            <li className="menu-item">Skills</li>
+                        </Link>
+                        <Link spy={true} smooth={true} to="Projects">
+                            <li className="menu-item">Projects</li>
+                        </Link>
+                        <Link spy={true} smooth={true} to="Favourite">
+                            <li className="menu-item">Favourite</li>
+                        </Link>
+                        <Link spy={true} smooth={true} to="Contact">
+                            <li className="menu-item">Contact</li>
+                        </Link>
+                </ul>
+            </div>
+
         </div>
     </>
     return (
         <nav>
             <div className="h-10vh flex justify-between z-50 text-black lg:py-5 px-20 py-4">
-                <div className="flex items center flex-1">
-                    <span className="text-2xl font-bold">Sofi Abovyan</span>
-                </div>
                 <div className="lg:flex md:flex lg:flex-1 items center justify-end font-normal hidden">
                     <div className="flex-10">
                         <ul className="flex gap-8 mr-16 text-[18px]">
